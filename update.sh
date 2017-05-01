7fc00a35a66e1dfc0fc11f7e53e30fcc9fd92007
Matus Tejiscak <ziman@functor.sk>
2017-05-01 19:59:00 +0100

    Add very primitive OS-sensitivity.

--- update.sh
+++ update.sh
@@ -7,6 +7,7 @@ SRC="$PWD/src"
 ROOT="$PWD/build"
 OPT_INCLUDE="/opt/local/include"
 OPT_LIB="/opt/local/lib"
+platform="$(uname -s)"
 
 mkdir -p "$SRC"
 mkdir -p "$ROOT"
@@ -17,15 +18,23 @@ mkdir -p "$ROOT"
 if ! [ -d "$SRC/hlhdf" ]; then
     cd "$SRC"; git clone git://git.baltrad.eu/hlhdf.git
     cd "$SRC/hlhdf"
-    ./configure \
-        --prefix="$ROOT/hlhdf" \
-        --with-hdf5="${OPT_INCLUDE},${OPT_LIB}"
+    if [ "$platform" = "Darwin" ]; then
+        ./configure \
+            --prefix="$ROOT/hlhdf" \
+            --with-hdf5="${OPT_INCLUDE},${OPT_LIB}"
+    elif [ "$platform" = "Linux" ]; then
+        ./configure \
+            --prefix="$ROOT/hlhdf" \
+            --with-hdf5="/usr/include/hdf5/serial,/usr/lib/x86_64-linux-gnu/hdf5/serial/"
+    fi
 fi
 
 cd "$SRC/hlhdf"
 git fetch origin
-git checkout --theirs -f origin/master
-sed -i '' -e 's/-bundle/-dynamiclib/g' def.mk
+git checkout -f origin/master
+if [ "$platform" = "Darwin" ]; then
+    sed -i '' -e 's/-bundle/-dynamiclib/g' def.mk
+fi
 make
 make install
 
@@ -77,7 +86,7 @@ if ! [ -d "$SRC/vol2bird" ]; then
 else
     cd "$SRC/vol2bird"
     git fetch origin
-    git checkout --theirs -f origin/master
+    git checkout -f origin/master
 fi
 
 cd "$SRC/vol2bird"
@@ -89,7 +98,9 @@ LDFLAGS="-L/opt/local/lib" CFLAGS="-I/opt/local/include" ./configure \
     --with-gsl="${OPT_INCLUDE}/gsl,${OPT_LIB}"
 
 # apply patch
-sed -i '' -e 's/-arch i386//g' def.mk
+if [ "$platform" = "Darwin" ]; then
+    sed -i '' -e 's/-arch i386//g' def.mk
+fi
 
 cd "$SRC/vol2bird"
 make
